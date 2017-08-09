cb5815cf68e00dec199c93130aeed9ccfe870b49
skye <skye53653@gmail.com>
2017-08-09 13:15:44 +0900

    can merge 3 images

--- stitching1.py
+++ stitching1.py
@@ -1,29 +1,55 @@
 import cv2
 import numpy as np
-files = ["fig3.jpg","fig2.jpg"]
+
+files = ["1Hill.JPG","2Hill.JPG","3Hill.JPG"]
 MIN_MATCH_COUNT = 4
-# get keypoint and descriptor for each image
-lst = []
+
+def drawMatch(img1,img2,kp1,kp2,good,mask):
+	w = img1.shape[1] 
+	vis = np.zeros((max(img1.shape[0],img2.shape[0]),img1.shape[1] + img2.shape[1],3),dtype="uint8") 
+	vis[:,0:img1.shape[1],:] = img1
+	vis[:,img1.shape[1]:] = img2
+	
+
+	for (m,s) in zip(good,mask):
+		if s:
+			
+
+			pt1 = (int(kp1[m.queryIdx].pt[0]),int(kp1[m.queryIdx].pt[1]))
+			pt2 = (int(kp2[m.trainIdx].pt[0]+w),int(kp2[m.trainIdx].pt[1]))
+			cv2.line(vis, pt1, pt2, (0, 255, 0), 1)
+
+	cv2.imwrite("match2.jpg",vis)
+# get detector and descriptor 
+
 detector = cv2.FeatureDetector_create("SIFT")
 extractor = cv2.DescriptorExtractor_create("SIFT")
+
+imgs = []
 for file in files:
 	img = cv2.imread(file)
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	print(img.shape)
+	imgs.append(img)
+
+
+for i in range(len(imgs)-1):
+	
+	img1 = imgs[i]
+	gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 	
 	# finds the keypoint in the image
 	kps = detector.detect(gray)
 	
 	# get descriptor
-	(kps,des) = extractor.compute(gray,kps)
+	(kp1,des1) = extractor.compute(gray,kps)
 	
-	lst.append((img,kps,des))
-
-for i in range(len(lst)-1):
+	img2 = imgs[i+1]
+	gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 	
-	kp1 = lst[i][1]
-	kp2 = lst[i+1][1]
-	des1 = lst[i][2]
-	des2 = lst[i+1][2]
+	# finds the keypoint in the image
+	kps = detector.detect(gray)
+	
+	(kp2,des2) = extractor.compute(gray,kps)
 
 	# match key point
 	bf = cv2.BFMatcher()
@@ -34,17 +60,25 @@ for i in range(len(lst)-1):
 		if m.distance < 0.75*n.distance:
 			good.append(m)
 
-
+	print(len(good))
 	if len(good) > MIN_MATCH_COUNT:
-		src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-		dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-		#dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+		src_pts = np.float32([ kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+		dst_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+		
 		H,mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-
-
+	drawMatch(img1,img2,kp1,kp2,good,mask)
 	# warp image
-	result = cv2.warpPerspective(lst[i][0],H,(1000,1000))
-	img2 = lst[i+1][0]
-	result[0:img2.shape[0], 0:img2.shape[1]] = img2
+	result = cv2.warpPerspective(img2,H,(150+imgs[i].shape[1],img1.shape[0]))
+	
+	result[0:img1.shape[0], 0:img1.shape[1]] = img1
+	imgs[i+1] = result
 	cv2.imwrite("result.jpg",result)
+'''
+result = np.zeros((imgs[0].shape[0],3000,3),dtype="uint8")
+print(result.shape)
+result[:,0:400] = imgs[0]
+result[:,400:700] = imgs[1]
+cv2.imwrite("result.jpg",result)
+'''
+
